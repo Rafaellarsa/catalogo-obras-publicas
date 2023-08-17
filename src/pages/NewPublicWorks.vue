18890337bb00a83c1c4a5321edba9c82c3256784
Rafaella Sampaio <sampaiorafaella@hotmail.com>
2023-08-17 16:08:19 -0300

    add NewPublicWorks content

--- src/pages/NewPublicWorks.vue
+++ src/pages/NewPublicWorks.vue
@@ -2,14 +2,120 @@
   <q-page>
     <div style="max-width: 500px" class="q-pa-md q-mx-auto">
       <h1>Adicionar obra</h1>
+
+      <q-input
+        filled
+        v-model="location"
+        label="Selecionar localização"
+        class="q-mb-sm"
+      />
+
+      <h3>Obras próximas</h3>
+
+      <h3>Dados da obra</h3>
+
+      <q-input filled v-model="name" label="Nome da obra" class="q-mb-sm" />
+      <div style="display: flex; justify-content: space-between">
+        <q-select
+          filled
+          v-model="category"
+          :options="categories"
+          label="Categoria"
+          class="q-mb-sm"
+          style="width: 55%"
+        />
+        <q-select
+          filled
+          v-model="state"
+          :options="states"
+          label="UF"
+          class="q-mb-sm"
+          style="width: 40%"
+        />
+      </div>
+      <q-input filled v-model="city" label="Cidade" class="q-mb-sm" />
+      <q-input
+        filled
+        v-model="startDate"
+        label="Data de início"
+        mask="##/##/####"
+        :rules="['date']"
+        class="q-mb-sm"
+      >
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy transition-show="scale" transition-hide="scale">
+              <q-date v-model="startDate" mask="DD/MM/YYYY">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Fechar" color="primary" flat />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+      <q-input
+        filled
+        v-model="deadline"
+        label="Prazo da obra"
+        mask="##/##/####"
+        :rules="['date']"
+        class="q-mb-sm"
+      >
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy transition-show="scale" transition-hide="scale">
+              <q-date v-model="deadline" mask="DD/MM/YYYY">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Fechar" color="primary" flat />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+
+      <q-input filled v-model="budget" label="Orçamento" class="q-mb-sm" />
+
+      <h3>Galeria da obra</h3>
+
+      <q-btn
+        to="/encontre-uma-obra"
+        color="primary"
+        label="Cadastrar Obra"
+        class="full-width q-mb-md"
+      />
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'NewPublicWorks',
+  setup() {
+    return {
+      location: ref(''),
+      name: ref(''),
+      category: ref(null),
+      state: ref(null),
+      city: ref(''),
+      startDate: ref(''),
+      deadline: ref(''),
+      budget: ref(''),
+      categories: ['Mobilidade Urbana', 'Turismo', 'Infraestrutura'],
+      states: ['CE', 'PI', 'RJ'],
+    };
+  },
 });
 </script>
+
+<style lang="sass" scoped>
+h3
+  font-size: 1rem
+  line-height: 1.25rem
+  font-weight: 700
+  width: 100%
+  margin-bottom: 0.5rem
+</style>
